4a5b1f2968b6374e539da577321784592f8e90e4
Lauren Kruse <laurenjmkruse@gmail.com>
2021-03-15 20:49:03 -0400

    Finalized app.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Day-Planner
+# 05 Third-Party APIs: Work Day Scheduler
+
+## User Story
+*AS AN employee with a busy schedule,
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively*
+
+### Build
+* Program assembled using HTML, CSS, JavaScript code
+* Libraries used: jQuery and Moment.js
+* HTML and CSS dynamically updated via JavaScript
+* Application deployed at live URL
+* Data stored locally by clicking save button
+
+### Purpose
+* Application provides user with means to plan their work day
+
+## Installation
+* Installation not required; program runs in browser
+* Deployed application may be found here:  https://laurenjmkruse.github.io/Day-Planner/
+
+
+## Credits
+To assemble this project, I relied upon the following:
+* Coding Bootcamp classes taught by Michael Labieniec
+* W3Schools: https://www.w3schools.com/
+* MDN web docs: https://developer.mozilla.org/en-US/
+* jQuery: https://jquery.com/
+* Moment.js: https://momentjs.com/
+* Background was taken from the Metropolitan Museum of Art
+* Fonts: https://fonts.google.com/
+* Save Button Icon: https://fontawesome.com/
--- script.js
+++ script.js
@@ -1,104 +1,77 @@
-// *************************************************
-// I. jQuery ready() method ensures HTML document loads prior to execution of JavaScript
+// ready() prevents page from loading before jQuery is ready
 $(document).ready(function() {
 
-    // *************************************************
-    // II. Global variables
-    const fullPage = $('body');
-    fullPage.addClass('full-page');
-    const calendarSpace = $('.container');
-    let dayName = moment().day(String);
-    let monthName = moment().month(String);
-    let currentDay = moment().date(Number);
-    let year = moment().year(Number);
-    let datePlaceholder = $('#currentDay');
-    datePlaceholder.css({marginTop: '50px', fontFamily: 'Eagle Lake', fontSize: '30px'}).text('Thursday, 18 February 2021');
-    console.log(dayName);
-    // datePlaceholder.text(`${dayName}, ${currentDay} ${monthName} ${year}`);
-    let userName = '';
+    // GLOBAL VARIABLES
+    let datePlaceholder = $("#currentDay");
+    let todaysDate = moment().format("dddd, Do MMMM YYYY");
+    let currentHour;
+    let hourBlock;
+    let itemDescription;
 
-    // *************************************************
-    // III. Array
-    let hoursArray = ['8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+    // Initial set-up
 
-    // *************************************************
-    // IV. try...catch to log any errors that may occur
-    try {
+    function init() {
+        // Display date
+        datePlaceholder.text(todaysDate).css({fontFamily: 'Eagle Lake', fontSize: '30px', marginTop: '20px'});
 
-        // *************************************************
-        // V. Functions
+        // Populate stored items
+        renderStoredItems();        
+    };
 
-        // A. Begin or refresh application
-        function initializePlanner() {
-            userName = 'Suzette';
-            let personalizedHeader = $('.display-3');
-            if (userName === '') {
-                personalizedHeader.text('Day Planner');
+    // Event listener to detect save button clicks
+    $(".saveBtn").on("click", function() {
+
+        // Collect information entered by user
+        hourBlock = $(this).parent().attr("id");
+        itemDescription = $(this).siblings(".description").val();
+
+        // Save user input to local storage
+        localStorage.setItem(hourBlock, itemDescription);
+    });
+
+
+    // 
+    function hourTracker() {
+        // Obtain current hour
+        currentHour = moment().hours();
+        console.log(currentHour);
+
+        // Determine timeblock colors
+        $(".time-block").each(function() {
+            let timeBlockHour = $(this).attr("id");
+            console.log(`Hour: ${timeBlockHour}`);
+
+            if (timeBlockHour < currentHour) {
+                $(this).addClass("past");
+            } else if (timeBlockHour == currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
             } else {
-                personalizedHeader.text(`${userName}'s Day Planner`);
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
             }
-            // let userName = ''; //prompt('Please enter your name (up to 12 characters): ');
-            
-            
-            buildTimeBlocks();
-        };
+        });
 
-        function buildTimeBlocks() {
-            console.log('Creating time blocks!');
-                calendarSpace.css('padding', '0');
-                let calendarPlaceholder = $('<div>').addClass('schedule-details');
-                calendarSpace.append(calendarPlaceholder);
-
-                let schedulePlaceholder = $('<div>').addClass('hour-details');
-                calendarPlaceholder.append(schedulePlaceholder);
-
-            $.each(hoursArray, function (index, hourNumber)
-            {
-                let hourBeingBuilt = hourNumber;
-                let hourSequence = index;
-                console.log('Hour number ' + hourSequence + ' is ' + hourBeingBuilt + ':00');
-
-                let timeBlock = $('<div>');
-                timeBlock.addClass('time-block row');
-                schedulePlaceholder.append(timeBlock);
-                
-                let hourColumn = $('<div>');
-                hourColumn.css('width', '15%');
-                hourColumn.css('marginBottom', '0');
-                let hourPlaceholder = $('<p>');
-                hourPlaceholder.addClass('hour').val(hourNumber).text(hourNumber + ':00');
-                hourColumn.append(hourPlaceholder);
-                timeBlock.append(hourColumn);
-                console.log('The value of this hour is ' + hourNumber);
-
-                let descriptionColumn = $('<div>');
-                descriptionColumn.css({width: '75%', borderWidth: '6px', borderLeftStyle: 'double', borderColor: 'black'});
-                let descriptionPlaceholder = $('<textarea>');
-                descriptionPlaceholder.addClass('description');
-                descriptionColumn.append(descriptionPlaceholder);
-                timeBlock.append(descriptionColumn);
-
-                let buttonColumn = $('<div>');
-                buttonColumn.css('width', '10%');
-                let saveButton = $('<button>');
-                saveButton.addClass('saveBtn fa fa-save i:hover');
-                buttonColumn.append(saveButton);              
-                timeBlock.append(buttonColumn);
-            });
-
-            schedulePlaceholder.on('click', function(event){
-                let currentButton = $(event.target);
-                let editedHour = currentButton.val();
-                console.log(`currentButton has value of ${editedHour}`);    
-            });
-        };
-
-        // *************************************************
-        // VI. Initial process
-        initializePlanner();
-
-       // end of try...catch to log any errors that may occur 
-    } catch (error) {
-        console.log('The following error occured: ', error);
+    
     }
+
+    function renderStoredItems() {
+        $("#08 .description").val(localStorage.getItem("08"));
+        $("#09 .description").val(localStorage.getItem("09"));
+        $("#10 .description").val(localStorage.getItem("10"));
+        $("#11 .description").val(localStorage.getItem("11"));
+        $("#12 .description").val(localStorage.getItem("12"));
+        $("#13 .description").val(localStorage.getItem("13"));
+        $("#14 .description").val(localStorage.getItem("14"));
+        $("#15 .description").val(localStorage.getItem("15"));
+        $("#16 .description").val(localStorage.getItem("16"));
+        $("#17 .description").val(localStorage.getItem("17"));
+         
+    };
+
+    // Processes
+    init();
+    hourTracker();
+
 });
